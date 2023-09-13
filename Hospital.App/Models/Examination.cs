541ddceb1859bf973621e0bca78297a8c5178cb2
Starinin Andrey <starininandrey@gmail.com>
2023-09-13 20:13:47 +0300

    Добавление конструктора в класс Examenation

--- Hospital.App/Models/Examination.cs
+++ Hospital.App/Models/Examination.cs
@@ -5,4 +5,11 @@ public class Examination
     public DateTime Date { get; init; }
     public Doctor Doctor { get; init; }
     public string Commentary { get; init; }
+
+    public Examination(DateTime date, Doctor doctor, string commentary)
+    {
+        Date = date;
+        Doctor = doctor;
+        Commentary = commentary;
+    }
 }
