ecfb91e297d173b1063f164c5074c116b8af0d3a
Starinin Andrey <starininandrey@gmail.com>
2023-09-15 20:34:11 +0300

    Добавление классов onsoleHelper и HospitalView

---
+++ Hospital.App/VL/ConsoleHelper.cs
@@ -0,0 +1,59 @@
+﻿namespace Hospital.App;
+
+public static class ConsoleHelper
+{
+    private static void PrintLine(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+    
+    private static void Print(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(message);
+        Console.ResetColor();
+    }
+
+    #region Print
+
+    public static void PrintInfo(string message)
+    {
+        PrintLine(message, ConsoleColor.Blue);
+    }
+    
+    public static void PrintError(string message)
+    {
+        PrintLine(message, ConsoleColor.Red);
+    }
+    
+    public static void PrintWarning(string message)
+    {
+        PrintLine(message, ConsoleColor.Yellow);
+    }
+    
+    public static void PrintSuccess(string message)
+    {
+        PrintLine(message, ConsoleColor.Green);
+    }
+
+    #endregion
+
+    #region Input
+
+    public static string Input(string message)
+    {
+        Print(message, ConsoleColor.Cyan);
+        return Console.ReadLine();
+    }
+    
+    public static int InputNumber(string message)
+    {
+        Print(message, ConsoleColor.Cyan);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    #endregion
+    
+}
---
+++ Hospital.App/VL/HospitalView.cs
@@ -0,0 +1,48 @@
+﻿using Hospital.App.Models;
+
+namespace Hospital.App;
+
+public static class HospitalView
+{
+    public static void ShowDoctor(Doctor doctor)
+    {
+        ConsoleHelper.PrintInfo($"{doctor.Id}: {doctor.FullName}, {doctor.Age}, {doctor.Sex}, {doctor.Specialization}");
+    }
+    
+    public static void ShowDoctors(IEnumerable<Doctor> doctors)
+    {
+        foreach (var doctor in doctors)
+        {
+            ShowDoctor(doctor);
+        }
+    }
+
+    public static void ShowExamination(Examination examination)
+    {
+        ConsoleHelper.PrintInfo($"{examination.Date} : {examination.Doctor.FullName} - {examination.Commentary}");
+    }
+    
+    public static void ShowMedicalCard(MedicalCard medicalCard)
+    {
+        ConsoleHelper.PrintInfo($"{medicalCard.Id}");
+        foreach (var examination in medicalCard.Examinations)
+        {
+            Console.Write("\t");
+            ShowExamination(examination);
+        }
+    }
+    
+    public static void ShowPatient(Patient patient)
+    {
+        ConsoleHelper.PrintInfo($"{patient.Id}: {patient.FullName}, {patient.Age}, {patient.Sex} - {patient.MedicalPolicyNumber}");
+        ShowMedicalCard(patient.MedicalCard);
+    }
+
+    public static void ShowPatients(IEnumerable<Patient> patients)
+    {
+        foreach (var patient in patients)
+        {
+            ShowPatient(patient);
+        }
+    }
+}
