84593ae37fd13d9f96780467d9e1bfb92701f8b9
Starinin Andrey <starininandrey@gmail.com>
2023-09-15 20:08:55 +0300

    Добавление класса Hospital

---
+++ Hospital.App/BLL/Hospital.cs
@@ -0,0 +1,32 @@
+﻿using Hospital.App.DAL;
+using Hospital.App.Models;
+
+namespace Hospital.App.BLL;
+
+public class Hospital
+{
+    private readonly IContext _context;
+
+    public Hospital(IContext context)
+    {
+        _context = context;
+        
+        _context.ImportDoctors();
+        _context.ImportPatients();
+    }
+
+    #region Doctors
+
+    public IEnumerable<Doctor> GetAllDoctors() => _context.Doctors;
+    public IEnumerable<Doctor> FindDoctorsByName(string name) => _context.Doctors.Where(d => d.FullName.Contains(name));
+
+    #endregion
+
+    #region Patients
+
+    public IEnumerable<Patient> GetAllPatients() => _context.Patients;
+
+    #endregion
+    
+    
+}
