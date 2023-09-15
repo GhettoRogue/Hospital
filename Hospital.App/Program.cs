3975a5286d63e878e6e61a7a2786f18a30a40113
Starinin Andrey <starininandrey@gmail.com>
2023-09-15 21:11:06 +0300

    Демонстрация работы

--- Hospital.App/Program.cs
+++ Hospital.App/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hospital.App.DAL;
+
+namespace Hospital.App;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var pathDoctors = "doctors.json";
+        var pathPatients = "patients.json";
+
+        var hospital = new BLL.Hospital(new MemoryContext());
+        
+        var doctors = hospital.GetAllDoctors();
+        HospitalView.ShowDoctors(doctors);
+        Console.WriteLine();
+
+
+        var patients = hospital.GetAllPatients();
+        HospitalView.ShowPatients(patients);
+        Console.WriteLine();
+
+        var name = ConsoleHelper.Input("Введите имя для поиска: ");
+        var docs = hospital.FindDoctorsByName(name);
+        HospitalView.ShowDoctors(docs);
+    }
+}
+
